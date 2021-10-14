4c577c70bcb5e690b59686e460b1862b5eaad774
Christian rios <cgrios@email.sc.edu>
2021-10-13 20:25:23 -0400

    Filled in MilitaryCadence

--- MilitaryCadence.java
+++ MilitaryCadence.java
@@ -1,26 +1,60 @@
-import java.util.concurrent.TimeUnit;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class MilitaryCadence {
-    public MilitaryCadence() {}
+    
+    public MilitaryCadence() {
+
+    }
     public void singIDontKnow() {
         try {
-            
-        } catch (Exception e) {
-            //TODO: handle exception
+            File file = new File("command-design-pattern/idontknow.txt");
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+            scanner.close();
+        } 
+        catch(FileNotFoundException e) {
+            System.out.println("IDontKnow cadence file not found");
+            e.printStackTrace();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
         }
     }
     public void singEverywhereWeGo() {
         try {
-            
-        } catch (Exception e) {
-            //TODO: handle exception
+            File file = new File("command-design-pattern/everywhere.txt");
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+            scanner.close();
+        } 
+        catch(FileNotFoundException e) {
+            System.out.println("Everywhere cadence file not found");
+            e.printStackTrace();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
         }
     }
     public void singInArmy() {
         try {
-            
-        } catch (Exception e) {
-            //TODO: handle exception
+            File file = new File("command-design-pattern/inarmy.txt");
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+            scanner.close();
+        } 
+        catch(FileNotFoundException e) {
+            System.out.println("Sing in Army cadence file not found");
+            e.printStackTrace();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
         }
     }
 }
