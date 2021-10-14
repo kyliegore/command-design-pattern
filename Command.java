4aa5a415fd9c308cd9d7fcf2ffda731fda44872f
Christian rios <cgrios@email.sc.edu>
2021-10-13 22:14:37 -0400

    added Command javadoc

--- Command.java
+++ Command.java
@@ -1,3 +1,7 @@
 public interface Command {
+    /**
+     * the execute command, implemented by the separate command classes, will access the sing methods in MilitaryCadence
+     * and allow the terminal to output the songs.
+     */
     public void execute();
 }
